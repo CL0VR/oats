5b9276993cd582455f12fec1f268876a43812de6
Dobbs <zdobbs24@gmail.com>
2016-01-29 11:12:38 -0700

    add gulp

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require ('gulp');
+
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task ('watch', function(){
+  gulp.watch ('./scss/**/*.scss', ['sass']);
+})
